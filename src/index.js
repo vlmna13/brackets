602583e206a374f302b97449c9b3096c4e509707
vlmna13 <>
2022-02-15 23:03:08 +0300

    add: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  let closingBrackets = {};
+
+  bracketsConfig.forEach((config) => {
+    closingBrackets[config[1]] = config[0];
+  });
+
+  for(let i = 0; i < str.length; i++) {
+    let letter = str[i];
+
+    if (closingBrackets.hasOwnProperty(letter)) {
+
+      let stackLastChar = stack[stack.length - 1];
+      let openingBracket = closingBrackets[letter];
+
+      if (letter === openingBracket) {
+        if (stackLastChar === letter) {
+          stack.pop()
+        } else {
+          stack.push(letter);
+        }
+        continue;
+      }
+      
+      if (openingBracket === stackLastChar) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    } else {
+      stack.push(letter);
+    }
+  }
+
+  return stack.length === 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
